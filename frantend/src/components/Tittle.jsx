c0f3bbcd27d0265c70c4aad8ba5aff3d5a527d46
vraj-chovatiya-11 <vrajchovatiya11@gmail.com>
2025-04-26 12:43:04 +0530

    update design of collection page

--- frantend/src/components/Tittle.jsx
+++ frantend/src/components/Tittle.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const Tittle = ({text1,text2}) => {
+const Tittle = ({text1, text2}) => {
   return (
-    <div className='inline-flex gap-2 items-center mb-3'>
-      <p className='text-gray-500'>{text1} <span className='text-gray-700 font-medium'>{text2}</span></p>
-      <p className='w-8 sm:w-12 h-[1px] bg-gray-700'></p>
+    <div className='inline-flex items-center'>
+      <div className='flex flex-col sm:flex-row sm:items-center gap-1 sm:gap-2'>
+        <h2 className='text-base sm:text-xl font-medium'>
+          <span className='text-gray-500'>{text1}</span>
+          {text2 && <span className='text-gray-800 ml-1'>{text2}</span>}
+        </h2>
+        <div className='hidden sm:block w-8 h-[1px] bg-gray-300'></div>
+      </div>
     </div>
   )
 }
